645239bdc0fa64df7b9abe2feb3165ac1b558e81
Chi-Fu Wang <chifuwang@yahoo.com>
2024-12-01 16:38:31 -0600

    rename test file name

--- src/test/java/com/example/springboot/SpringBootTestingApplicationITCase.java
+++ src/test/java/com/example/springboot/SpringBootTestingApplicationITCase.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootTestingApplicationTests {
+class SpringBootTestingApplicationITCase {
 
 	@Test
 	void contextLoads() {
